b7d03eaeb5b5da56e4db285193216da24df451a1
rajasekar.kulasekaran <rajaseka.kulasekaran@homeadvisor.com>
2022-02-02 11:53:45 -0600

    update push4

--- src/main/java/stepDefinitions/GetExchangeRatesForHistoricalDateSteps.java
+++ src/main/java/stepDefinitions/GetExchangeRatesForHistoricalDateSteps.java
@@ -1,99 +1,99 @@
-package main.java.stepDefinitions;
-
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import io.restassured.response.Response;
-import org.testng.Assert;
-import static org.hamcrest.Matchers.*;
-import static io.restassured.RestAssured.get;
-import main.java.utils.Utils;
-
-
-public class GetExchangeRatesForHistoricalDateSteps {
-
-    private String apiForHistoricalExchangeRate;
-    private String apiForFutureExchangeRate;
-    private String incorrectApiForHistoricalExchangeRate;
-    private Response response;
-    private final Utils utils;
-
-    //Getting utils with common methods using Cucumber's dependency injection
-    public GetExchangeRatesForHistoricalDateSteps(Utils utils){
-
-        this.utils = utils;
-    }
-
-
-    //Test exchange rate api service for old date with correct url
-    @Given("^Rest api service url with old date is given$")
-    public void rest_api_service_url_with_old_date_is_given() {
-        apiForHistoricalExchangeRate = utils.baseUri() + utils.getOldDate();
-    }
-    @When("^Rest api service url with old date is called$")
-    public void rest_api_service_url_with_old_date_is_called()  {
-        response = get(apiForHistoricalExchangeRate);
-    }
-    @Then("^Assert the status code for old date is 200$")
-    public void assert_the_status_code_for_old_date_is_200() {
-        Assert.assertEquals(response.getStatusCode(),200);
-    }
-
-    @Then("^Assert the response for old date$")
-    public void assert_the_response_for_old_date() {
-        get(apiForHistoricalExchangeRate).then()
-                .contentType("application/json")
-                .body("base",equalTo("EUR"))
-                .body("date",equalTo(utils.getOldDate()))
-                .body("rates",notNullValue());
-        for(int i=0;i<utils.getCurrencyList().length;i++){
-            get(apiForHistoricalExchangeRate).then().body("rates",hasKey(utils.getCurrencyList()[i]));
-        }
-    }
-
-
-    //Test exchange rate api service for future date with correct url
-    @Given("^Rest api service url with future date is given$")
-    public void rest_api_service_url_with_future_date_is_given() {
-        apiForFutureExchangeRate = utils.baseUri() + utils.getFutureDate();
-    }
-    @When("^Rest api service url with future date is called$")
-    public void rest_api_service_url_with_future_date_is_called()  {
-        response = get(apiForFutureExchangeRate);
-    }
-    @Then("^Assert the status code for future date is 200$")
-    public void assert_the_status_code_for_future_date_is_200() {
-        Assert.assertEquals(response.getStatusCode(),200);
-    }
-    @Then("^Assert the date is equal to current date in response$")
-    public void assert_the_date_is_equal_to_current_date_in_response() {
-        get(apiForFutureExchangeRate).then()
-                .contentType("application/json")
-                .body("base",equalTo("EUR"))
-                .body("date",equalTo(utils.getCurrentDate()))
-                .body("rates",notNullValue());
-        for(int i=0;i<utils.getCurrencyList().length;i++){
-            get(apiForFutureExchangeRate).then().body("rates",hasKey(utils.getCurrencyList()[i]));
-        }
-    }
-
-
-    //Test exchange rate api service for old date with incorrect url
-    @Given("^Rest api service for old date is incorrect$")
-    public void rest_api_service_for_old_date_is_incorrect() {
-        incorrectApiForHistoricalExchangeRate = utils.baseUri() + "old/" + utils.getOldDate();
-    }
-
-    @When("^Incorrect rest api service for old date is called$")
-    public void incorrect_rest_api_service_for_old_date_is_called()  {
-        response = get(incorrectApiForHistoricalExchangeRate);
-    }
-
-    @Then("^Assert the status code for incorrect url for future date is 404$")
-    public void assert_the_status_code_for_incorrect_url_for_future_date_is_404() {
-        Assert.assertEquals(response.getStatusCode(), 404);
-
-    }
-
-    }
-
+//package main.java.stepDefinitions;
+//
+//import io.cucumber.java.en.Given;
+//import io.cucumber.java.en.Then;
+//import io.cucumber.java.en.When;
+//import io.restassured.response.Response;
+//import org.testng.Assert;
+//import static org.hamcrest.Matchers.*;
+//import static io.restassured.RestAssured.get;
+//import main.java.utils.Utils;
+//
+//
+//public class GetExchangeRatesForHistoricalDateSteps {
+//
+//    private String apiForHistoricalExchangeRate;
+//    private String apiForFutureExchangeRate;
+//    private String incorrectApiForHistoricalExchangeRate;
+//    private Response response;
+//    private final Utils utils;
+//
+//    //Getting utils with common methods using Cucumber's dependency injection
+//    public GetExchangeRatesForHistoricalDateSteps(Utils utils){
+//
+//        this.utils = utils;
+//    }
+//
+//
+//    //Test exchange rate api service for old date with correct url
+//    @Given("^Rest api service url with old date is given$")
+//    public void rest_api_service_url_with_old_date_is_given() {
+//        apiForHistoricalExchangeRate = utils.baseUri() + utils.getOldDate();
+//    }
+//    @When("^Rest api service url with old date is called$")
+//    public void rest_api_service_url_with_old_date_is_called()  {
+//        response = get(apiForHistoricalExchangeRate);
+//    }
+//    @Then("^Assert the status code for old date is 200$")
+//    public void assert_the_status_code_for_old_date_is_200() {
+//        Assert.assertEquals(response.getStatusCode(),200);
+//    }
+//
+//    @Then("^Assert the response for old date$")
+//    public void assert_the_response_for_old_date() {
+//        get(apiForHistoricalExchangeRate).then()
+//                .contentType("application/json")
+//                .body("base",equalTo("EUR"))
+//                .body("date",equalTo(utils.getOldDate()))
+//                .body("rates",notNullValue());
+//        for(int i=0;i<utils.getCurrencyList().length;i++){
+//            get(apiForHistoricalExchangeRate).then().body("rates",hasKey(utils.getCurrencyList()[i]));
+//        }
+//    }
+//
+//
+//    //Test exchange rate api service for future date with correct url
+//    @Given("^Rest api service url with future date is given$")
+//    public void rest_api_service_url_with_future_date_is_given() {
+//        apiForFutureExchangeRate = utils.baseUri() + utils.getFutureDate();
+//    }
+//    @When("^Rest api service url with future date is called$")
+//    public void rest_api_service_url_with_future_date_is_called()  {
+//        response = get(apiForFutureExchangeRate);
+//    }
+//    @Then("^Assert the status code for future date is 200$")
+//    public void assert_the_status_code_for_future_date_is_200() {
+//        Assert.assertEquals(response.getStatusCode(),200);
+//    }
+//    @Then("^Assert the date is equal to current date in response$")
+//    public void assert_the_date_is_equal_to_current_date_in_response() {
+//        get(apiForFutureExchangeRate).then()
+//                .contentType("application/json")
+//                .body("base",equalTo("EUR"))
+//                .body("date",equalTo(utils.getCurrentDate()))
+//                .body("rates",notNullValue());
+//        for(int i=0;i<utils.getCurrencyList().length;i++){
+//            get(apiForFutureExchangeRate).then().body("rates",hasKey(utils.getCurrencyList()[i]));
+//        }
+//    }
+//
+//
+//    //Test exchange rate api service for old date with incorrect url
+//    @Given("^Rest api service for old date is incorrect$")
+//    public void rest_api_service_for_old_date_is_incorrect() {
+//        incorrectApiForHistoricalExchangeRate = utils.baseUri() + "old/" + utils.getOldDate();
+//    }
+//
+//    @When("^Incorrect rest api service for old date is called$")
+//    public void incorrect_rest_api_service_for_old_date_is_called()  {
+//        response = get(incorrectApiForHistoricalExchangeRate);
+//    }
+//
+//    @Then("^Assert the status code for incorrect url for future date is 404$")
+//    public void assert_the_status_code_for_incorrect_url_for_future_date_is_404() {
+//        Assert.assertEquals(response.getStatusCode(), 404);
+//
+//    }
+//
+//    }
+//
--- src/main/java/stepDefinitions/GetExchangeRatesForLatestDateSteps.java
+++ src/main/java/stepDefinitions/GetExchangeRatesForLatestDateSteps.java
@@ -1,73 +1,73 @@
-package main.java.stepDefinitions;
-
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.When;
-import io.cucumber.java.en.Then;
-import static io.restassured.RestAssured.get;
-import io.restassured.response.Response;
-import org.testng.Assert;
-import static org.hamcrest.Matchers.*;
-import main.java.utils.Utils;
-
-
-public class GetExchangeRatesForLatestDateSteps {
-
-    private String apiForLatestExchangeRate;
-    private String incorrectApiForLatestExchangeRate;
-    private Response response;
-    private final Utils utils;
-
-    //Getting utils with common methods using Cucumber's dependency injection
-    public GetExchangeRatesForLatestDateSteps(Utils utils){
-
-        this.utils = utils;
-    }
-
-
-    //Test exchange rate api service for current date with correct url
-    @Given("^Rest api service for latest date is available$")
-    public void rest_api_service_for_latest_date_is_available() {
-        apiForLatestExchangeRate = utils.baseUri() + "latest";
-    }
-
-    @When("^Rest api service for latest date is called$")
-    public void rest_api_service_for_latest_date_is_called()  {
-        response = get(apiForLatestExchangeRate);
-    }
-
-    @Then("^Assert the status code for current date is 200$")
-    public void assert_the_status_code_for_current_date_is_200() {
-
-        Assert.assertEquals(response.getStatusCode(), 200);
-    }
-
-    @Then("^Assert the response for the current date$")
-    public void assert_the_response_for_the_current_date() {
-        get(apiForLatestExchangeRate).then()
-                .contentType("application/json")
-                .body("base",equalTo("EUR"))
-                .body("date",equalTo(utils.getCurrentDate()))
-                .body("rates",notNullValue());
-        for(int i=0;i<utils.getCurrencyList().length;i++){
-            get(apiForLatestExchangeRate).then().body("rates",hasKey(utils.getCurrencyList()[i]));
-        }
-    }
-
-    //Test exchange rate api service for current date with incorrect url
-    @Given("^Rest api service for latest date is incorrect$")
-    public void rest_api_service_for_latest_date_is_incorrect() {
-        incorrectApiForLatestExchangeRate = utils.baseUri() + "late";
-    }
-
-    @When("^Incorrect rest api service for latest date is called$")
-    public void incorrect_rest_api_service_for_latest_date_is_called()  {
-        response = get(incorrectApiForLatestExchangeRate);
-    }
-
-    @Then("^Assert the status code for wrong url for current date is 400$")
-    public void assert_the_status_code_for_wrong_url_for_current_date_is_400() {
-        Assert.assertEquals(response.getStatusCode(), 400);
-
-    }
-}
-
+//package main.java.stepDefinitions;
+//
+//import io.cucumber.java.en.Given;
+//import io.cucumber.java.en.When;
+//import io.cucumber.java.en.Then;
+//import static io.restassured.RestAssured.get;
+//import io.restassured.response.Response;
+//import org.testng.Assert;
+//import static org.hamcrest.Matchers.*;
+//import main.java.utils.Utils;
+//
+//
+//public class GetExchangeRatesForLatestDateSteps {
+//
+//    private String apiForLatestExchangeRate;
+//    private String incorrectApiForLatestExchangeRate;
+//    private Response response;
+//    private final Utils utils;
+//
+//    //Getting utils with common methods using Cucumber's dependency injection
+//    public GetExchangeRatesForLatestDateSteps(Utils utils){
+//
+//        this.utils = utils;
+//    }
+//
+//
+//    //Test exchange rate api service for current date with correct url
+//    @Given("^Rest api service for latest date is available$")
+//    public void rest_api_service_for_latest_date_is_available() {
+//        apiForLatestExchangeRate = utils.baseUri() + "latest";
+//    }
+//
+//    @When("^Rest api service for latest date is called$")
+//    public void rest_api_service_for_latest_date_is_called()  {
+//        response = get(apiForLatestExchangeRate);
+//    }
+//
+//    @Then("^Assert the status code for current date is 200$")
+//    public void assert_the_status_code_for_current_date_is_200() {
+//
+//        Assert.assertEquals(response.getStatusCode(), 200);
+//    }
+//
+//    @Then("^Assert the response for the current date$")
+//    public void assert_the_response_for_the_current_date() {
+//        get(apiForLatestExchangeRate).then()
+//                .contentType("application/json")
+//                .body("base",equalTo("EUR"))
+//                .body("date",equalTo(utils.getCurrentDate()))
+//                .body("rates",notNullValue());
+//        for(int i=0;i<utils.getCurrencyList().length;i++){
+//            get(apiForLatestExchangeRate).then().body("rates",hasKey(utils.getCurrencyList()[i]));
+//        }
+//    }
+//
+//    //Test exchange rate api service for current date with incorrect url
+//    @Given("^Rest api service for latest date is incorrect$")
+//    public void rest_api_service_for_latest_date_is_incorrect() {
+//        incorrectApiForLatestExchangeRate = utils.baseUri() + "late";
+//    }
+//
+//    @When("^Incorrect rest api service for latest date is called$")
+//    public void incorrect_rest_api_service_for_latest_date_is_called()  {
+//        response = get(incorrectApiForLatestExchangeRate);
+//    }
+//
+//    @Then("^Assert the status code for wrong url for current date is 400$")
+//    public void assert_the_status_code_for_wrong_url_for_current_date_is_400() {
+//        Assert.assertEquals(response.getStatusCode(), 400);
+//
+//    }
+//}
+//
